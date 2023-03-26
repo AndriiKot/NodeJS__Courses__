24d6c972218b25e76f6d3805cc36fd0a4c73acfd
AndriiKot <a_kocyuba@ukr.net>
2023-03-26 08:55:07 +0200

    add port 8000

--- demo2/index.js
+++ demo2/index.js
@@ -1,1 +1,10 @@
-﻿
+﻿const http = require('http')
+const port = 8000
+
+const server = http.createServer((req,res) => {
+  res.end("Hello, World!");  
+})
+
+server.listen(port, () => {
+  console.log('Server listering on: http://localhost:%s',port);
+})
