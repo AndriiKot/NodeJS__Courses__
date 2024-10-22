8a414af1ba38ff2e0c13b62eae8c1ab6898c6433
AndriiKot <a_kocyuba@ukr.net>
2024-10-22 20:39:31 +0200

    NodeJS and Express 'loftblog' ! create Server Express atrists id  !

--- loftblog/_02_Express-Server/server.js
+++ loftblog/_02_Express-Server/server.js
@@ -1,8 +1,36 @@
 const express = require("express");
 const app = express();
 
+const artists = [
+  {
+    id: 1,
+    name: "Metallica",
+    genre: "Heavy Metal",
+  },
+  {
+    id: 2,
+    name: "AC/DC",
+    genre: "Hard Rock",
+  },
+  {
+    id: 3,
+    name: "Nirvana",
+    genre: "Grunge",
+  },
+];
+
+app.get("/artists", (req, res) => {
+  res.send(artists);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello API!");
+});
+
+app.get("/artists/:id", (req, res) => {
+  const id = req.params.id;
+  const artist = artists.find((a) => a.id === +id);
+  res.send(artist);
 });
 
 app.listen(3000, () => {
